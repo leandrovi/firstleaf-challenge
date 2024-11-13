12b2ab32214d8b6b1d2495dcac4b92ad847cd16a
leandrovi <leandrofernandesvieira@gmail.com>
2024-11-12 22:46:57 -0300

    test: mock analytics

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,13 @@
 import "@testing-library/jest-dom";
 
+declare global {
+  interface Window {
+    analytics: {
+      track: (eventName: string, params: Record<string, any>) => void;
+    };
+  }
+}
+
 window.HTMLElement.prototype.scrollIntoView = function () {};
 
 jest.mock("lottie-web", () => ({
@@ -7,3 +15,11 @@ jest.mock("lottie-web", () => ({
     destroy: jest.fn(),
   }),
 }));
+
+window.analytics = {
+  track: jest.fn(),
+};
+
+beforeEach(() => {
+  (window.analytics.track as jest.Mock).mockClear();
+});
